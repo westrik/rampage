7c6ee4537c86891b24c9f87792da387b293d1942
Matthew Westrik <m@ttwestrik.com>
2019-07-25 23:30:59 -0700

    output empty 500x500 image to png

--- src/bin/rampage.rs
+++ src/bin/rampage.rs
@@ -1,5 +1,8 @@
 extern crate rampage;
 
+use rampage::output_image;
+
 fn main() {
     println!("Hello, world!");
+    output_image();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,19 @@ pub mod color;
 pub mod geometry;
 pub mod material;
 pub mod output;
+
+use image::png::PNGEncoder as PngEncoder;
+use image::ColorType;
+use std::fs::File;
+use std::path::Path;
+
+pub fn output_image() {
+    let width = 500 as u32;
+    let height = 500 as u32;
+    let pixels: Vec<_> = (0..(width * height * 3)).map(|_| 0).collect::<Vec<_>>();
+
+    let path = Path::new("output.png");
+    PngEncoder::new(File::create(path).expect("could not open file"))
+        .encode(&pixels, width, height, ColorType::RGB(8))
+        .expect("could not output png")
+}
